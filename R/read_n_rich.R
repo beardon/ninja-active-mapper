49f5a1b2f1d0653d15b8a1adec0601d2eff87769
Phillip D. Alderman <phillip.alderman@okstate.edu>
2021-01-05 12:09:09 -0600

    Working version of read_n_rich_strip_farmer_practice() that has new argument percentile_threshold that gives the percentile at which N rich strip summary value should be calculated. Needs validation with real data.

--- R/read_n_rich.R
+++ R/read_n_rich.R
@@ -2,6 +2,11 @@
 #' 
 #' @param shapefile_path a length-one character vector that gives the file path
 #'   for the shapefile for the N rich strip
+#' @param active_ndvi a raster layer of actively mapped NDVI
+#' @param shapefile_crs a coordinate reference system description as generated
+#'   by `crs()`
+#' @param percentile_threshold percentile at which to calculate the summary
+#'   value for the N Rich Strip (percent)
 #'   
 #' @export
 #' 
@@ -40,7 +45,9 @@ read_n_rich_strip_farmer_practice <- function(shapefile_path,active_ndvi,
     }
   }
   
-  avg_nr_ndvi <- raster::extract(active_ndvi,n_rich,quantile,na.rm=TRUE,probs=percentile_threshold/100) %>% 
+  avg_nr_ndvi <- raster::extract(active_ndvi,n_rich,
+                                 function(x,na.rm=TRUE){quantile(x, na.rm = TRUE,
+                                                                 probs = percentile_threshold/100)}) %>% 
     as.vector() # return of raster::extract() is matrix need to convert to length-one vector
   avg_fp_ndvi <- raster::extract(active_ndvi,farmer_practice,mean,na.rm=TRUE) %>% 
     as.vector() # return of raster::extract() is matrix need to convert to length-one vector
